a034c2fb0c3db0d280086ca14deb104a8499c671
BogoCoder <samuelperez.di@gmail.com>
2019-09-02 12:02:54 -0500

    trying 4

--- Taller 4/taller4bsol.sql
+++ Taller 4/taller4bsol.sql
@@ -11,7 +11,8 @@ with recursive etapas_post(prodid, etapaid, posterior) as (
 select * from etapas_post order by prodid, etapaid,posterior;
 
 --3
-create or replace function maxVentas() returns table(prodid numeric(4,0), fecha date, volventas numeric(4,0))
+create or replace function maxVentas() 
+returns table(prodid numeric(4,0), fecha date, volventas numeric(4,0))
 as
 $$
 BEGIN
@@ -23,3 +24,19 @@ $$
 language 'plpgsql' STABLE;
 
 --4
+
+select prodid, max(volventas) over (partition by prodid) as maxtot, 
+ntile(4) over (partition by prodid order by (volventas) asc) as cuartil, 
+percentile_disc(array[0.25,0.5,0.75,1]) within group(order by prodid,volventas) from ventasdiarias;
+
+create or replace function quartil() 
+returns table(prodid numeric(4,0), fecha date, volventas numeric(4,0))
+as
+$$
+BEGIN
+	return query
+	(select * from ventasdiarias order by volventas DESC limit 5);
+
+END;
+$$
+language 'plpgsql' STABLE;
